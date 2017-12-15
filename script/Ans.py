5b959dadfdca03009d1d909a7889e2179147d472
pokemonchw <admin@byayoi>
2017-12-15 16:51:14 +0800

    角色创建demo

--- script/Ans.py
+++ script/Ans.py
@@ -6,12 +6,12 @@ import core.EraPrint as eprint
 logomenu = ['1','2','3']
 yesorno = ['4','5','12']
 inputNickNameList = ['4','5','14','12']
-sex = ['6','7','8','9','10','11','13','12']
+sex = ['6','7','9','15','13','12']
 
 def option(inputI):
     for i in range(0,len(inputI)):
         id = int(inputI[i])
         pycmd.pcmd(text.loadCmdAdv(inputI[i]),id,None)
         eprint.p('\n')
-    ans = game.askfor_int()
+    ans = game.askfor_int(inputI)
     return ans
