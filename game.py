06e9eedb996211f37f03d3485dcaac18fa941e9f
byayoi <pokemonchw@gmail.com>
2020-05-06 01:18:38 +0800

    新增debug模式

--- game.py
+++ game.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 
+import getopt
+import sys
 from Script.Core import game_data
 
-game_data.init()
+try:
+    options, _ = getopt.getopt(sys.argv[1:], "d:", ["debug="])
+    key, value = options[0]
+    if key in ("-d", "--debug") and value[1] == "1":
+        game_data.init(1)
+    else:
+        game_data.init(0)
+except:
+    game_data.init(0)
 
 from Script.Design import start_flow
 from Script.Core import game_init
