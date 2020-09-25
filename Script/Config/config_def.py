479f53de41720ea1e77c7aedf142fda2bc07ab9c
byayoi <nekoharuya@byayoi.org>
2020-09-25 19:42:47 +0800

    更新数据表

--- Script/Config/config_def.py
+++ Script/Config/config_def.py
@@ -1,10 +1,88 @@
-class ClothingType:
-    """ 衣服种类配置 """
+class SexTem:
+    """ 性别对应描述和性别器官模板 """
 
     cid: int
-    """ 类型id """
+    """ 性别id """
     name: str
-    """ 类型名字 """
+    """ 性别名称 """
+    has_man_organ: bool
+    """ 是否有男性器官 """
+    has_woman_organ: bool
+    """ 是否有女性器官 """
+
+
+class BarConfig:
+    """ 比例条名字对应的状态图片和绘制宽度 """
+
+    cid: int
+    """ 比例条id """
+    name: str
+    """ 比例条名字 """
+    ture_bar: str
+    """ 进度条图片 """
+    null_bar: str
+    """ 背景条图片 """
+    width: int
+    """ 图片绘制宽度 """
+
+
+class Organ:
+    """ 器官对应性别限定和文字描述 """
+
+    cid: int
+    """ 器官id """
+    organ_type: int
+    """ 类型(0:女,1:男,2:通用) """
+    name: str
+    """ 名字 """
+
+
+class AgeJudgeSexExperienceTem:
+    """ 不同性别不同年龄段对应生成不同性经验模板的权重 """
+
+    cid: int
+    """ 模板id """
+    sex: int
+    """ 性别类型 """
+    age: int
+    """ 年龄段 """
+    sex_exp_tem: int
+    """ 性经验模板 """
+    weight: int
+    """ 权重 """
+
+
+class HitPointTem:
+    """ hp模板对应平均值 """
+
+    cid: int
+    """ 模板id """
+    max_value: int
+    """ 最大值 """
+
+
+class WeightTem:
+    """ 体重模板对应体重范围 """
+
+    cid: int
+    """ 模板id """
+    min_value: float
+    """ 最小值 """
+    max_value: float
+    """ 最大值 """
+
+
+class ChestTem:
+    """ 罩杯对应范围和生成权重 """
+
+    cid: int
+    """ 模板id """
+    min_value: float
+    """ 最小值 """
+    max_value: float
+    """ 最大值 """
+    weight: int
+    """ 权重 """
 
 
 class FontConfig:
@@ -34,6 +112,75 @@ class FontConfig:
     """ 备注 """
 
 
+class ClothingCollocational:
+    """ 搭配模板 """
+
+    cid: int
+    """ 模板id """
+    clothing_tem_type: int
+    """ 搭配所属服装种类 """
+    clothing_type: int
+    """ 当前服装类型 """
+    clothing_tem: int
+    """ 当前服装种类 """
+    collocational: int
+    """ 搭配限制0:不限制1:优先2:禁用3:仅穿或不穿4:必须5:必须为所有此项之一 """
+    info: str
+    """ 备注 """
+
+
+class EndAgeTem:
+    """ 性别对应平均寿命 """
+
+    cid: int
+    """ 模板id """
+    sex: int
+    """ 性别id """
+    end_age: int
+    """ 平均寿命 """
+
+
+class ClothingTem:
+    """ 服装模板 """
+
+    cid: int
+    """ 模板id """
+    name: str
+    """ 服装名字 """
+    clothing_type: int
+    """ 服装类型 """
+    sex: int
+    """ 服装性别限制 """
+    tag: int
+    """ 服装用途标签 """
+    describe: str
+    """ 描述 """
+
+
+class FoodQualityWeight:
+    """ 烹饪技能等级制造食物品质权重配置 """
+
+    cid: int
+    """ 配置表id """
+    level: int
+    """ 烹饪技能等级 """
+    quality: int
+    """ 食物品质 """
+    weight: int
+    """ 权重 """
+
+
+class RandomNpcSexWeight:
+    """ 生成随机npc时性别权重 """
+
+    cid: int
+    """ 模板id """
+    sex: int
+    """ 性别id """
+    weight: int
+    """ 权重 """
+
+
 class ManaPointTem:
     """ mp模板对应平均值 """
 
@@ -43,17 +190,28 @@ class ManaPointTem:
     """ 最大值 """
 
 
-class SexTem:
-    """ 性别对应描述和性别器官模板 """
+class AgeTem:
+    """ 不同年龄段模板的年龄范围 """
 
     cid: int
-    """ 性别id """
-    name: str
-    """ 性别名称 """
-    has_man_organ: bool
-    """ 是否有男性器官 """
-    has_woman_organ: bool
-    """ 是否有女性器官 """
+    """ 模板id """
+    max_age: int
+    """ 最大年龄 """
+    min_age: int
+    """ 最小年龄 """
+
+
+class SexExperience:
+    """ 性经验丰富程度模板对应器官性经验模板 """
+
+    cid: int
+    """ 表id """
+    sex_exp_type: int
+    """ 经验类型 """
+    organ_id: int
+    """ 器官id """
+    exp_tem: int
+    """ 经验模板 """
 
 
 class AttrTem:
@@ -92,93 +250,22 @@ class SexExperienceTem:
     """ 最小经验 """
 
 
-class ChestTem:
-    """ 罩杯对应范围和生成权重 """
+class ClothingType:
+    """ 衣服种类配置 """
 
     cid: int
-    """ 模板id """
-    min_value: float
-    """ 最小值 """
-    max_value: float
-    """ 最大值 """
-    weight: int
-    """ 权重 """
-
-
-class WeightTem:
-    """ 体重模板对应体重范围 """
-
-    cid: int
-    """ 模板id """
-    min_value: float
-    """ 最小值 """
-    max_value: float
-    """ 最大值 """
-
-
-class BarConfig:
-    """ 比例条名字对应的状态图片和绘制宽度 """
-
-    cid: int
-    """ 比例条id """
+    """ 类型id """
     name: str
-    """ 比例条名字 """
-    ture_bar: str
-    """ 进度条图片 """
-    null_bar: str
-    """ 背景条图片 """
-    width: int
-    """ 图片绘制宽度 """
+    """ 类型名字 """
 
 
-class Organ:
-    """ 器官对应性别限定和文字描述 """
+class ClothingUseType:
+    """ 服装用途配置 """
 
     cid: int
-    """ 器官id """
-    organ_type: int
-    """ 类型(0:女,1:男,2:通用) """
+    """ 用途id """
     name: str
-    """ 名字 """
-
-
-class AgeTem:
-    """ 不同年龄段模板的年龄范围 """
-
-    cid: int
-    """ 模板id """
-    max_age: int
-    """ 最大年龄 """
-    min_age: int
-    """ 最小年龄 """
-
-
-class AgeJudgeSexExperienceTem:
-    """ 不同性别不同年龄段对应生成不同性经验模板的权重 """
-
-    cid: int
-    """ 模板id """
-    sex: int
-    """ 性别类型 """
-    age: int
-    """ 年龄段 """
-    sex_exp_tem: int
-    """ 性经验模板 """
-    weight: int
-    """ 权重 """
-
-
-class SexExperience:
-    """ 性经验丰富程度模板对应器官性经验模板 """
-
-    cid: int
-    """ 表id """
-    sex_exp_type: int
-    """ 经验类型 """
-    organ_id: int
-    """ 器官id """
-    exp_tem: int
-    """ 经验模板 """
+    """ 用途名字 """
 
 
 class BodyFatTem:
@@ -194,39 +281,3 @@ class BodyFatTem:
     """ 最小值 """
     max_value: float
     """ 最大值 """
-
-
-class EndAgeTem:
-    """ 性别对应平均寿命 """
-
-    cid: int
-    """ 模板id """
-    sex: int
-    """ 性别id """
-    end_age: int
-    """ 平均寿命 """
-
-
-class ClothingTem:
-    """  """
-
-
-
-class HitPointTem:
-    """ hp模板对应平均值 """
-
-    cid: int
-    """ 模板id """
-    max_value: int
-    """ 最大值 """
-
-
-class RandomNpcSexWeight:
-    """ 生成随机npc时性别权重 """
-
-    cid: int
-    """ 模板id """
-    sex: int
-    """ 性别id """
-    weight: int
-    """ 权重 """
--- Script/Config/game_config.py
+++ Script/Config/game_config.py
@@ -10,20 +10,60 @@ config_normal = game_type.NormalConfig()
 """ 游戏通用配置数据 """
 config_data = {}
 """ 原始json数据 """
-config_font:Dict[int,config_def.FontConfig] = {}
-""" 字体配置数据 """
-config_font_data:Dict[str,int] = {}
-""" 字体名字对应字体id """
+config_age_judge_sex_experience_tem:Dict[int,config_def.AgeJudgeSexExperienceTem] = {}
+""" 不同性别不同年龄段对应生成不同性经验模板的权重 """
+config_age_judge_sex_experience_tem_data:Dict[int,Dict[int,Dict[int,int]]]
+"""
+不同性别不同年龄段对应生成不同性经验模板的配置数据
+性别:年龄段:性经验模板:权重
+"""
+config_age_tem:Dict[int,config_def.AgeTem] = {}
+""" 年龄段年龄范围模板 """
+config_attr_tem:Dict[int,config_def.AttrTem] = {}
+""" 性别对应角色各项基础属性模板 """
 config_bar:Dict[int,config_def.BarConfig] = {}
 """ 比例条配置数据 """
 config_bar_data:Dict[str,int] = {}
 """ 比例条名字对应比例条id """
+config_body_fat_tem:Dict[int,config_def.BodyFatTem] = {}
+""" 按性别划分的体脂率模板和范围 """
+config_body_fat_tem_data:Dict[int,Dict[int,int]] = {}
+"""
+性别划分的体脂率模板对应范围数据
+性别:体脂率范围:体脂率范围配置id
+"""
 config_chest:Dict[int,config_def.ChestTem] = {}
 """ 罩杯配置数据 """
-config_attr_tem:Dict[int,config_def.AttrTem] = {}
-""" 性别对应角色各项基础属性模板 """
-config_age_tem:Dict[int,config_def.AgeTem] = {}
-""" 年龄段年龄范围模板 """
+config_clothing_collocational:[int,config_def.ClothingCollocational] = {}
+""" 服装搭配模板配置数据 """
+config_clothing_tem:[int,config_def.ClothingTem]
+""" 服装模板配置数据 """
+config_clothing_type:[int,config_def.ClothingType]
+""" 衣服种类配置数据 """
+config_clothing_use_type:[int,config_def.ClothingUseType]
+""" 衣服用途配置数据 """
+config_end_age_tem[int,config_def.EndAgeTem] = {}
+""" 最终年龄范围配置模板 """
+config_font:Dict[int,config_def.FontConfig] = {}
+""" 字体配置数据 """
+config_font_data:Dict[str,int] = {}
+""" 字体名字对应字体id """
+config_hitpoint_tem:Dict[int,config_def.HitPointTem] = {}
+""" HP模板对应平均值 """
+config_manapoint_tem:Dict[int,config_def.ManaPointTem] = {}
+""" MP模板对应平均值 """
+config_organ:Dict[int,config_def.Organ] = {}
+""" 器官种类配置 """
+config_random_sex_weight[int,config_def.RandomNpcSexWeight] = {}
+""" 生成随机npc时性别权重配置数据 """
+config_sex_experience[int,config_def.SexExperience] = {}
+""" 性经验丰富程度模板对应器官性经验模板 """
+config_sex_experience_tem[int,config_def.SexExperienceTem] = {}
+""" 器官类型性经验丰富程度对应经验范围 """
+config_sex_tem[int,config_def.SexTem] = {}
+""" 性别对应描述和性别器官模板 """
+config_weight_tem[int,config_def.WeightTem] = {}
+""" 体重模板对应体重范围 """
 
 
 def init_normal_config():
@@ -44,13 +84,31 @@ def load_data_json():
     config_data = json_handle.load_json(data_path)
 
 
-def load_font_data():
-    """ 载入字体配置数据 """
-    for font_data in config_data["FontConfig"]["data"]:
-        now_font = config_def.FontConfig()
-        now_font.__dict__ = font_data
-        config_font[now_font.cid] = now_font
-        config_font_data[now_font.name] = now_font.cid
+def load_age_judge_sex_experience_tem_data():
+    """ 载入不同性别不同年龄段对应生成不同性经验模板的权重 """
+    for tem_data in config_data["AgeJudgeSexExperienceTem"]["data"]:
+        now_tem = config_def.AgeJudgeSexExperienceTem()
+        now_tem.__dict__ = tem_data
+        config_age_judge_sex_experience_tem[now_tem.cid] = now_tem
+        config_age_judge_sex_experience_tem_data.setdefault(now_tem.sex,{})
+        config_age_judge_sex_experience_tem_data[now_tem.sex].setdefault(now_tem.age,{})
+        config_age_judge_sex_experience_tem_data[now_tem.sex][now_tem.age][now_tem.sex_exp_tem] = now_tem.weight
+
+
+def load_age_tem():
+    """ 载入各年龄段对应年龄范围模板 """
+    for age_tem in config_data["AgeTem"]["data"]:
+        now_tem = config_def.AgeTem()
+        now_tem.__dict__ = age_tem
+        config_age_tem[now_tem.cid] = now_tem
+
+
+def load_attr_tem():
+    """ 载入性别对应角色各项基础属性模板 """
+    for attr_tem in config_data["AttrTem"]["data"]:
+        now_tem = config_def.AttrTem()
+        now_tem.__dict__ = attr_tem
+        config_attr_tem[now_tem.cid] = now_tem
 
 
 def load_bar_data():
@@ -62,6 +120,16 @@ def load_bar_data():
         config_bar_data[now_bar.name] = now_bar.cid
 
 
+def load_body_fat_tem():
+    """ 载入按性别划分的体脂率模板和范围配置数据 """
+    for tem_data in config_data["BodyFatTem"]["data"]:
+        now_tem = config_def.BodyFatTem()
+        now_tem.__dict__ = tem_data
+        config_body_fat_tem[now_tem.cid] = now_tem
+        config_body_fat_tem_data.setdefault(now_tem.sex_type,{})
+        config_body_fat_tem_data[now_tem.sex_type][now_tem.sub_type] = now_tem.cid
+
+
 def load_chest_tem_data():
     """ 载入罩杯配置数据 """
     for chest_data in config_data["ChestTem"]["data"]:
@@ -70,28 +138,140 @@ def load_chest_tem_data():
         config_chest[now_chest.cid] = now_chest
 
 
-def load_attr_tem():
-    """ 载入性别对应角色各项基础属性模板 """
-    for attr_tem in config_data["AttrTem"]["data"]:
-        now_tem = config_def.AttrTem()
-        now_tem.__dict__ = attr_tem
-        config_attr_tem[now_tem.cid] = now_tem
+def load_clothing_collocational():
+    """ 载入服装搭配模板配置数据 """
+    for tem_data in config_data["ClothingCollocational"]["data"]:
+        now_collocational = config_def.ClothingCollocational()
+        now_collocational.__dict__ = tem_data
+        config_clothing_collocational[now_collocational.cid] = now_collocational
 
 
-def load_age_tem():
-    """ 载入各年龄段对应年龄范围模板 """
-    for age_tem in config_data["AgeTem"]["data"]:
-        now_tem = config_def.AgeTem()
-        now_tem.__dict__ = age_tem
-        config_age_tem[now_tem.cid] = now_tem
+def load_clothing_tem():
+    """ 载入服装模板配置数据 """
+    for tem_data in config_data["ClothingTem"]["data"]:
+        now_tem = config_def.ClothingTem()
+        now_tem.__dict__ = tem_data
+        config_clothing_tem[now_tem.cid] = now_tem
+
+
+def load_clothing_type():
+    """ 载入衣服种类配置数据 """
+    for type_data in config_data["ClothingType"]["data"]:
+        now_type = config_def.ClothingType()
+        now_type.__dict__ = type_data
+        config_clothing_type[now_type.cid] = now_type
+
+
+def load_clothing_use_type():
+    """ 载入衣服用途配置数据 """
+    for type_data in config_data["ClothingUseType"]["data"]:
+        now_type = config_def.ClothingUseType()
+        now_type.__dict__ = type_data
+        config_clothing_use_type[now_type.cid] = now_type
+
+
+def load_end_age_tem():
+    """ 载入最终年龄范围配置模板 """
+    for tem_data in config_data["EndAgeTem"]["data"]:
+        now_tem = config_def.EndAgeTem()
+        now_tem.__dict__ = tem_data
+        config_end_age_tem[now_tem.cid] = now_tem
+
+
+def load_font_data():
+    """ 载入字体配置数据 """
+    for font_data in config_data["FontConfig"]["data"]:
+        now_font = config_def.FontConfig()
+        now_font.__dict__ = font_data
+        config_font[now_font.cid] = now_font
+        config_font_data[now_font.name] = now_font.cid
+
+
+def load_hitpoint_tem():
+    """ 载入hp模板对应平均值配置数据 """
+    for tem_data in config_data["HitPointTem"]["data"]:
+        now_tem = config_def.HitPointTem()
+        now_tem.__dict__ = tem_data
+        config_hitpoint_tem[now_tem.cid] = now_tem
+
+
+def load_manapoint_tem():
+    """ 载入mp模板对应平均值配置数据 """
+    for tem_data in config_data["ManaPointTem"]["data"]:
+        now_tem = config_def.ManaPointTem()
+        now_tem.__dict__ = tem_data
+        config_manapoint_tem[now_tem.cid] = now_tem
+
+
+def load_organ_data():
+    """ 载入器官种类配置 """
+    for tem_data in config_data["Organ"]["data"]:
+        now_tem = config_def.Organ()
+        now_tem.__dict__ = tem_data
+        config_organ[now_tem.cid] = now_tem
+
+
+def load_random_sex_weight():
+    """ 载入生成随机npc时性别权重配置数据 """
+    for tem_data in config_data["RandomNpcSexWeight"]["data"]:
+        now_tem = config_def.RandomNpcSexWeight()
+        now_tem.__dict__ = tem_data
+        config_random_sex_weight[now_tem.cid] = now_tem
+
+
+def load_sex_experience():
+    """ 载入性经验丰富模板对应器官性经验模板配置数据 """
+    for tem_data in config_data["SexExperience"]["data"]:
+        now_tem = config_def.SexExperience()
+        now_tem.__dict__ = tem_data
+        config_sex_experience[now_tem.cid] = now_tem
+
+
+def load_sex_experience_tem():
+    """ 载入器官类型性经验丰富程度对应经验范围 """
+    for tem_data in config_data["SexExperienceTem"]["data"]:
+        now_tem = config_def.SexExperienceTem()
+        now_tem.__dict__ = tem_data
+        config_sex_experience_tem[now_tem.cid] = now_tem
+
+
+def load_sex_tem():
+    """ 载入性别对应描述和性别器官模板数据 """
+    for tem_data in config_data["SexTem"]["data"]:
+        now_tem = config_def.SexTem()
+        now_tem.__dict__ = tem_data
+        config_sex_tem[now_tem.cid] = now_tem
+
+
+def load_weight_tem():
+    """ 载入体重木板对应体重范围 """
+    for tem_data in config_data["WeightTem"]["data"]:
+        now_tem = config_def.WeightTem()
+        now_tem.__dict__ = tem_data
+        config_weight_tem[now_tem.cid] = now_tem
 
 
 def init():
     """ 初始化游戏配置数据 """
     load_data_json()
     init_normal_config()
-    load_font_data()
-    load_bar_data()
-    load_chest_tem_data()
-    load_attr_tem()
+    load_age_judge_sex_experience_tem_data()
     load_age_tem()
+    load_attr_tem()
+    load_bar_data()
+    load_body_fat_tem()
+    load_chest_tem_data()
+    load_clothing_collocational()
+    load_clothing_tem()
+    load_clothing_type()
+    load_clothing_use_type()
+    load_end_age_tem()
+    load_font_data()
+    load_hitpoint_tem()
+    load_manapoint_tem()
+    load_organ_data()
+    load_random_sex_weight()
+    load_sex_experience()
+    load_sex_experience_tem()
+    load_sex_tem()
+    load_weight_tem()
