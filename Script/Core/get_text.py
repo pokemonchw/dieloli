0eb19308f39b612062f78336619473b65b986705
(ง •_•)ง <miswanting@users.noreply.github.com>
2020-11-22 17:25:00 +0800

    优化程序启动引导

--- Script/Core/get_text.py
+++ Script/Core/get_text.py
@@ -12,7 +12,11 @@ def getLocStrings() -> FunctionType:
     FunctionType -- 翻译处理函数对象
     """
     po_data = os.path.join("data", "po")
-    return gettext.translation("dieloli", po_data, [normal_config.config_normal.language, "zh_CN"]).gettext
+    try:
+        return gettext.translation("dieloli", po_data, [normal_config.config_normal.language, "zh_CN"]).gettext
+    except FileNotFoundError:
+        raise Exception(
+            "请先将{}中的 *.po 文件转换为 *.mo 文件！（推荐使用 Poedit 工具）".format(po_data))
 
 
 _: FunctionType = getLocStrings()
